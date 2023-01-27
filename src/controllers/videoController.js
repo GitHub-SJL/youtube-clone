05cf7ec55ce3e7c399de1251b9afd187698e78f8
Sangjune Lee <aeeou154@@naver.com>
2023-01-27 23:00:43 +0900

    @3 Async Await 설명

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,20 @@
 // 스키마 모델 불러오기
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  console.log("Start");
-  Video.find({}, (error, videos) => {
-    console.log("Finished");
-    return res.render("home", { pageTitle: "Home", videos });
-  });
-  console.log("I finish first");
+export const home = async (req, res) => {
+  console.log("i start")
+  // await이 database에게 결과값을 받을때 까지 기다려줌
+  // find는 callback을 필요로 하지 않는다는걸 알려줌
+  // 그래서 find는 찾아낸 비디오를 바로 출력해주게됨
+  // 만약 try catch를 사용하면 어떠한 에러가 발생 시 await 내 출력 값을 출력 안하고 에러 출력 코드를 실행
+  
+  const videos = await Video.find({});
+  console.log("i finish")
+  console.log(videos)
+  // i start
+  // i finish
+  // []
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
--- src/init.js
+++ src/init.js
@@ -11,6 +11,6 @@ const PORT = 4000;
 
 
 const handleListening = () =>
-  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+    console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
