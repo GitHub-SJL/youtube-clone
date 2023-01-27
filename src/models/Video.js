4c12ca0d9824d21ee2082170c253bd5ae810aa00
Sangjune Lee <aeeou154@@naver.com>
2023-01-28 00:15:51 +0900

    @3 Validation and Schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+    title: { type: String, required: true, trim: true, maxLength: 80 },
+    description: { type: String, required: true, trim: true, minLength: 10 },
     // date.now() 로하면 즉시 1번만 실행됨 
     // default로 설정하면 매번 따로 설정할 필요없어짐
     createdAt: { type: Date, required: true, default: Date.now },
-    hashtags: [{ type: String }],
+    hashtags: [{ type: String, trim: true }],
     meta: {
-      views: { type: Number, default: 0, required: true },
-      rating: { type: Number, default: 0, required: true },
+        views: { type: Number, default: 0, required: true },
+        rating: { type: Number, default: 0, required: true },
     },
-  });
+});
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
