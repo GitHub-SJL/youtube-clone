413f497bad3a81a83258daf6c0dad3c3525cc37e
Sangjune Lee <aeeou154@@naver.com>
2023-01-27 23:23:18 +0900

    @3 Creating a Video #1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,18 +2,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  console.log("i start")
-  // await이 database에게 결과값을 받을때 까지 기다려줌
-  // find는 callback을 필요로 하지 않는다는걸 알려줌
-  // 그래서 find는 찾아낸 비디오를 바로 출력해주게됨
-  // 만약 try catch를 사용하면 어떠한 에러가 발생 시 await 내 출력 값을 출력 안하고 에러 출력 코드를 실행
-  
   const videos = await Video.find({});
-  console.log("i finish")
-  console.log(videos)
-  // i start
-  // i finish
-  // []
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -35,6 +24,17 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title: title,
+    description: description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 };
