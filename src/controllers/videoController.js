71968de18f790f594bfcf43b763a461cf5ae44d2
Sangjune Lee <aeeou154@@naver.com>
2023-01-27 23:41:06 +0900

    @3 Creating a Video #2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -23,7 +23,7 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
   const { title, description, hashtags } = req.body;
   const video = new Video({
     title: title,
@@ -35,6 +35,6 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
+  await video.save();
   return res.redirect("/");
 };
