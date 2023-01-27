97bfa75b545977f5d9a0e5048c53cdce41e6dde4
Sangjune Lee <aeeou154@@naver.com>
2023-01-28 00:32:49 +0900

    @3 Video Detail

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -8,8 +8,8 @@ import {
   } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+//몽고 db가 만들어주는 id 형식을 맞추기 위한 정규표현식
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 export default videoRouter;
