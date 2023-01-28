1d1bf49f5d624e1f67a415034aa47874242d6b72
Sangjune Lee <aeeou154@@naver.com>
2023-01-28 09:10:26 +0900

    @4 Create Account #1

--- src/init.js
+++ src/init.js
@@ -1,8 +1,6 @@
-// server.js에서 관리하지만 관련없던 코드들을 분리했음
-
 import "./db";
-// db를 mongoose와 성공적으로 연결시킨 후 model들을 인식
 import "./models/Video";
+import "./models/User";
 import app from "./server"
 
 const PORT = 4000;
