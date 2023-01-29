ece16be117177cf196bbd81eefd570d8e6a6256b
Sangjune Lee <aeeou154@@naver.com>
2023-01-29 09:57:25 +0900

    @5 Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,4 +5,22 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
     next();
-}; 
+};
+
+// 로그인된 사용자만 접근할수있도록
+export const protectorMiddleware = (req, res, next) => {
+    if (req.session.loggedIn) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};
+
+// 로그인 하지않은 사용자만 접근할수있도록
+export const publicOnlyMiddleware = (req, res, next) => {
+    if (!req.session.loggedIn) {
+        return next();
+    } else {
+        return res.redirect("/");
+    }
+};
